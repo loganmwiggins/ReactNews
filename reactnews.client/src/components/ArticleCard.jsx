96fa63526bb7e35690ca9185db938a061cd5c562
Logan Wiggins <loganmwiggins1@gmail.com>
2025-01-10 11:39:28 -0500

    ArticleCard styling changes

--- reactnews.client/src/components/ArticleCard.jsx
+++ reactnews.client/src/components/ArticleCard.jsx
@@ -9,16 +9,26 @@ function ArticleCard({imagePath, title, author, dateTime, description, url}) {
 
     return (
         <div className="news-item" onClick={() => openArticle(url)}>
-            <img className="news-image" src={imagePath} alt="Article image" draggable="false" />
-            <div className="news-title">{title}</div>
-            
-            <div className="news-meta">
-                <span className="news-author">{author}</span> &bull;
-                <span className="news-date">{dateTime.toString()}</span>
+            <div className="news-item-content">
+                <img className="news-image" src={imagePath} alt="Article image" draggable="false" />
+
+                <div className="news-text">
+                    <div className="news-meta">
+                        { author ? (<span className="news-author">{author} • </span>) : (<span></span>) }
+                        <span className="news-date">{new Date(dateTime).toLocaleDateString()}</span>
+                    </div>
+
+                    <div className="news-title">{title}</div>
+                    <p className="news-description">{description}</p>
+                </div>
             </div>
-            <p className="news-description">{description}</p>
+            <div className="news-item-btns">
+                <button type="button" className="btn-icon">
+                    <img src="/assets/heart.svg" draggable="false" />
+                </button>
+            </div>
         </div>
     );
 }
 
-export default ArticleCard
+export default ArticleCard;
